6a17a79ed8253bdffac9e13f82bbb8f353a5607b
Никита Зозуля <nik.zozulya.04@mail.ru>
2023-09-16 13:29:51 +0300

    Renamed method name 'String' to 'StringAsList' and added new method 'StringAsTable'

--- converter/ramustypes/box.go
+++ converter/ramustypes/box.go
@@ -17,17 +17,20 @@ func (is IsMainProcess) Process() string {
 type Box struct {
 	Id              int
 	Name            string
+	Reference       string
 	InputArrows     []*InputArrow
 	OutputArrows    []*OutputArrow
 	ControlArrows   []*ControlArrow
 	MechanismArrows []*MechanismArrow
 }
 
-func NewBox(id int, name string) *Box {
+func NewBox(id int, name string, reference string) *Box {
 	name = strings.ReplaceAll(name, "<CR>", " ")
+	name = strings.ReplaceAll(name, "'", "")
 	return &Box{
 		Id:              id,
 		Name:            name,
+		Reference:       reference,
 		InputArrows:     make([]*InputArrow, 0),
 		OutputArrows:    make([]*OutputArrow, 0),
 		ControlArrows:   make([]*ControlArrow, 0),
@@ -56,14 +59,14 @@ func (box Box) AddArrow(arrow Arrow, tp string) *Box {
 
 const BoxPrefix = "{LWI I 4 255 255 255}"
 
-func (box Box) String() string {
+func (box Box) StringAsList() string {
 	controlStr := "Управляющим потоком (потоком Управления) является:"
 	mechString := "Процесс осуществляется (механизм):"
 	inputStr := "Входами процесса являются:"
 	outputString := "Выходом процесса является:"
 
 	for i, arr := range box.ControlArrows {
-		controlStr += " " + Arrow(*arr).String()
+		controlStr += " " + Arrow(*arr).StringAsList()
 		if i == len(box.ControlArrows)-1 {
 			controlStr += "."
 		} else {
@@ -72,7 +75,7 @@ func (box Box) String() string {
 	}
 
 	for i, arr := range box.MechanismArrows {
-		mechString += " " + Arrow(*arr).String()
+		mechString += " " + Arrow(*arr).StringAsList()
 		if i == len(box.MechanismArrows)-1 {
 			mechString += "."
 		} else {
@@ -81,7 +84,7 @@ func (box Box) String() string {
 	}
 
 	for i, arr := range box.InputArrows {
-		inputStr += " " + Arrow(*arr).String()
+		inputStr += " " + Arrow(*arr).StringAsList()
 		if i == len(box.InputArrows)-1 {
 			inputStr += "."
 		} else {
@@ -90,7 +93,7 @@ func (box Box) String() string {
 	}
 
 	for i, arr := range box.OutputArrows {
-		outputString += " " + Arrow(*arr).String()
+		outputString += " " + Arrow(*arr).StringAsList()
 		if i == len(box.OutputArrows)-1 {
 			outputString += "."
 		} else {
@@ -98,9 +101,32 @@ func (box Box) String() string {
 		}
 	}
 
-	return fmt.Sprintf("**Процесс %s**:\n\n"+
+	return fmt.Sprintf("**Процесс \"%s\"**:\n\n"+
 		"+ %s\n\n"+
 		"+ %s\n\n"+
 		"+ %s\n\n"+
 		"+ %s\n", box.Name, controlStr, mechString, inputStr, outputString)
+}
+
+func (box Box) StringAsTable() string {
+	var control, mech, input, output strings.Builder
+
+	for i, arrow := range box.InputArrows {
+		input.WriteString(fmt.Sprintf("%s I%d\t", arrow.Label, i))
+	}
+
+	for i, arrow := range box.OutputArrows {
+		output.WriteString(fmt.Sprintf("%s O%d\t", arrow.Label, i))
+	}
+
+	for i, arrow := range box.MechanismArrows {
+		mech.WriteString(fmt.Sprintf("%s M%d\t", arrow.Label, i))
+	}
+
+	for i, arrow := range box.ControlArrows {
+		control.WriteString(fmt.Sprintf("%s C%d\t", arrow.Label, i))
+	}
+
+	return fmt.Sprintf("|%s %s|%s|%s|%s|%s|", box.Name, box.Reference,
+		input.String(), output.String(), mech.String(), control.String())
 }
