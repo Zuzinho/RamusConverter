e92c7a6fa7fbb2e773cac17878b8bc4c315d0249
Никита Зозуля <nik.zozulya.04@mail.ru>
2023-09-16 13:40:39 +0300

    Added new common func convert, type for converters funcs and new url pattern handler

--- main.go
+++ main.go
@@ -7,7 +7,10 @@ import (
 	"net/http"
 	"ramus/checker"
 	"ramus/converter"
+	"strings"
 )
+
+const tmpl = "./templates/index.tmpl"
 
 func main() {
 	port := "3000"
@@ -15,8 +18,6 @@ func main() {
 	log.Println("Opened server on port 3000")
 
 	mux := http.NewServeMux()
-
-	tmpl := "./templates/index.tmpl"
 
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		log.Println("server on '/'")
@@ -33,40 +34,51 @@ func main() {
 		}
 	})
 
-	mux.HandleFunc("/convert/", func(w http.ResponseWriter, r *http.Request) {
-		log.Println("server on '/convert/'")
-		ts, err := template.ParseFiles(tmpl)
-		if err != nil {
-			throwOnErr(err, &w)
-			return
-		}
+	mux.HandleFunc("/convert_list/", func(w http.ResponseWriter, r *http.Request) {
+		log.Println("server on '/convert_list/'")
+		convert(w, r, converter.ConvertAsList)
+	})
 
-		file, fileHeader, err := r.FormFile("file")
-		if err != nil {
-			throwOnErr(err, &w)
-			return
-		}
-
-		if ok, err := checker.IsCorrectFormat(fileHeader); !ok {
-			throwOnErr(err, &w)
-			return
-		}
-
-		rd, err := converter.Convert(file)
-		logOnErr(err)
-
-		buf, err := io.ReadAll(rd)
-		logOnErr(err)
-
-		err = ts.Execute(w, string(buf))
-		if err != nil {
-			throwOnErr(err, &w)
-			return
-		}
+	mux.HandleFunc("/convert_table/", func(w http.ResponseWriter, r *http.Request) {
+		log.Println("server on '/convert_table/'")
+		convert(w, r, converter.ConvertAsTable)
 	})
 
 	err := http.ListenAndServe(":"+port, mux)
 	logOnErr(err)
+}
+
+type convertFunc func(reader io.Reader) (*strings.Reader, error)
+
+func convert(w http.ResponseWriter, r *http.Request, convert convertFunc) {
+	ts, err := template.ParseFiles(tmpl)
+	if err != nil {
+		throwOnErr(err, &w)
+		return
+	}
+
+	file, fileHeader, err := r.FormFile("file")
+	if err != nil {
+		throwOnErr(err, &w)
+		return
+	}
+
+	if ok, err := checker.IsCorrectFormat(fileHeader); !ok {
+		throwOnErr(err, &w)
+		return
+	}
+
+	rd, err := convert(file)
+	logOnErr(err)
+
+	buf, err := io.ReadAll(rd)
+	logOnErr(err)
+
+	err = ts.Execute(w, string(buf))
+	if err != nil {
+		throwOnErr(err, &w)
+		return
+	}
 }
 
 func logOnErr(err error) {
